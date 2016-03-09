7572517df6b07555a3adadb71f0aee664b07f4f0
Vivek <vivekjha92@gmail.com>
2016-03-10 02:42:32 +0530

    Basic Structure created

---
+++ I Glove you v1/Assets/Scripts/SplashLogin.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashLogin : MonoBehaviour
+{
+    
+	void Start ()
+    {
+        Invoke("LoadMenu", 3f);
+	}
+	
+    void Update()
+    {
+        //just some loading effect //replace this before shipping
+        GetComponent<SpriteRenderer>().color = Color.Lerp(Color.white, Color.black, Mathf.PingPong(Time.time*5, 1f));
+    }
+
+    void LoadMenu()
+    {
+        SceneManager.LoadScene("main menu");
+    }
+}
